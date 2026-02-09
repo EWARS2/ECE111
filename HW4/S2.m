bc76fc59d149324c928d3ae2dd6594d34071853b
EWARS2 <jefmacho99@gmail.com>
2026-02-09 16:38:31 -0600

    fix: comment S2

--- HW4/S2.m
+++ HW4/S2.m
@@ -5,11 +5,11 @@ year = Data(:,3);             % Obtain 3rd column year.decimal_month (X)
 CO2 = Data(:,4);              % Obtain 4th column CO2 ppm (Y)
 B = [year.^2, year, year.^0]; % Setup for curve fit
 disp('Obtained [a;b;c] for CO2 = aX^2 + bX + c')
-format short e;
-A = inv(B'*B)*B'*CO2
+format short e; % Problem 4a
+A = inv(B'*B)*B'*CO2 
 plot(year,CO2,'b',year,B*A,'r')
 xlabel('year'); ylabel('CO2 ppm')
-format shortG
+format shortG % Problem 4b
 ppm400  = roots(A - [0;0;400])
 ppm800  = roots(A - [0;0;800])
 ppm1200 = roots(A - [0;0;1200])

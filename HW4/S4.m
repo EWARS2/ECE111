c2970b653da10baec865d9d2c0bba67f91653c42
EWARS2 <jefmacho99@gmail.com>
2026-02-09 15:53:19 -0600

    feat: S4 complete

--- HW4/S4.m
+++ HW4/S4.m
@@ -4,6 +4,7 @@ Data = readtable('4 SeaIce.txt', ...
     'Delimiter', {' ', '\t'}, 'MultipleDelimsAsOne', true);
 year = Data{:,1};
 Ice = Data{:,2};
+
 B = [year, year.^0]; % Problem 6a
 A = inv(B'*B)*B'*Ice
 subplot(211)
@@ -13,11 +14,10 @@ ylabel('m km^2');
 title('Arctic Sea Ice Area')
 roots(A) % Problem 6b
 
-
-B = [year.^2, year, year.^0];
+B = [year.^2, year, year.^0]; % Problem 7a
 A = inv(B'*B)*B'*Ice
 subplot(212)
 plot(year,Ice,'b',year,B*A,'r')
 xlabel('year');
 ylabel('Area (M km^2)')
-roots(A)
+roots(A) % Problem 7b

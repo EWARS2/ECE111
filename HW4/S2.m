10ebc92005cad33412b452f132e90b5bde33c17e
EWARS2 <jefmacho99@gmail.com>
2026-02-09 09:15:59 -0600

    feat: S2 done

--- HW4/S2.m
+++ HW4/S2.m
@@ -1,9 +1,11 @@
 format compact; clear; clc % Global CO2 Levels
+% Place data in same directory as this script
 Data = readmatrix('co2_mm_mlo.txt', "CommentStyle", "#");
 year = Data(:,3);             % Obtain 3rd column year.decimal_month (X)
 CO2 = Data(:,4);              % Obtain 4th column CO2 ppm (Y)
 B = [year.^2, year, year.^0]; % Setup for curve fit
-format short e; disp('Obtained [a;b;c] for CO2 = aX^2 + bX + c')
+disp('Obtained [a;b;c] for CO2 = aX^2 + bX + c')
+format short e;
 A = inv(B'*B)*B'*CO2
 plot(year,CO2,'b',year,B*A,'r')
 xlabel('year'); ylabel('CO2 ppm')

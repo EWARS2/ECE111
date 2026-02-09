169e58867cf624e212dd7f92245b0e183d9d6afb
EWARS2 <jefmacho99@gmail.com>
2026-02-09 16:30:04 -0600

    fix: beautify slightly

--- HW4/S3.m
+++ HW4/S3.m
@@ -6,9 +6,11 @@ year = Data{:,1}; % Note the curly braces. Needed for tables.
 T = Data{:,14};
 B = [year.^2, year, year.^0]; % Problem 5a
 A = inv(B'*B)*B'*T
+
 plot(year,T,'b',year,B*A,'r')
 xlabel('year');
 ylabel('Degrees F')
+
 Temp = @(y) [y^2 y 1] * A;
 T0 = Temp(1945) % Problem 5b
 T1 = Temp(2025)
--- HW4/S5.m
+++ HW4/S5.m
@@ -6,3 +6,40 @@ Data = readtable('4 Temperature Deviation.txt', ...
     'VariableNamesLine', 6, 'NumHeaderLines', 7, ...
     'LeadingDelimitersRule', 'ignore')
 
+year = Data(:,1); % Problem 8
+dT = Data(:,2);
+B = [year, year.^0];
+A = inv(B'*B)*B'*dT % 8a
+
+plot(year,dT,'b.-',year,B*A,'r')
+xlabel('year');
+ylabel('Degrees C')
+
+y = 2050;
+T2050 = [y, 1] * A
+
+y = 2100;
+T2100 = [y, 1] * A
+
+roots(A - [0;6])
+
+
+% Problem 9
+y = Data(:,1);
+dT = Data(:,2);
+B = [y.^2, y, y.^0];
+format short e
+A = inv(B'*B)*B'*dT
+
+plot(y,dT,'b',y,B*A,'r')
+xlabel('year')
+ylabel('C');
+title('Global Temperature Deviations')
+
+y = 2050;
+[y^2 y 1]*A
+
+y = 2100;
+[y^2 y 1]*A
+
+roots(A - [0;0;6])

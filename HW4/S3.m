07ce8ff53558092c970c51f568fc9707ccefd0e7
EWARS2 <jefmacho99@gmail.com>
2026-02-09 10:03:15 -0600

    feat: S3-a working

---
+++ HW4/S3.m
@@ -0,0 +1,13 @@
+format compact; clear; clc % Fargo Temperatures
+% Place data in same directory as this script
+filename = '4 Fargo_Weather_Monthly_Avg.txt';
+opts = detectImportOptions(filename);
+Data = readtable(filename, opts);
+Data = Data(~all(isnan(Data{:,:}), 2), :); % Discard all instances of NaN
+year = Data{:,1}; % Note the curly braces. Needed for tables.
+T = Data{:,14};
+B = [year.^2, year, year.^0];
+A = inv(B'*B)*B'*T
+plot(year,T,'b',year,B*A,'r')
+xlabel('year');
+ylabel('Degrees F')

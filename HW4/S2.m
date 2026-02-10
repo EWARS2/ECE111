9e35862c9809fa04b8b5becac6ed29633e66f3c5
EWARS2 <jefmacho99@gmail.com>
2026-02-09 22:05:16 -0600

    feat: cleanup and finish!

--- HW4/S2.m
+++ HW4/S2.m
@@ -4,9 +4,9 @@ Data = readmatrix('co2_mm_mlo.txt', "CommentStyle", "#");
 year = Data(:,3);             % Obtain 3rd column year.decimal_month (X)
 CO2 = Data(:,4);              % Obtain 4th column CO2 ppm (Y)
 B = [year.^2, year, year.^0]; % Setup for curve fit
-disp('Obtained [a;b;c] for CO2 = aX^2 + bX + c')
 format short e; % Problem 4a
-A = inv(B'*B)*B'*CO2 
+A = inv(B'*B)*B'*CO2
+
 plot(year,CO2,'b',year,B*A,'r')
 xlabel('year'); ylabel('CO2 ppm')
 format shortG % Problem 4b

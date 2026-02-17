54ac262c175394567ea7344bb553f2181935df87
EWARS2 <jefmacho99@gmail.com>
2026-02-17 10:53:05 -0600

    fix: cleanup

--- HW6/S3.m
+++ HW6/S3.m
@@ -22,8 +22,8 @@ ylim([-0.75,1.5])
 title("Problem 5")
 
 subplot(313) % 6)
-%q = piecewise(t<0, 0, 0<t & t<2, (t.^2)/8, 2<t & t<3, -0.5*t.^2 +3*t -3.5, t>3, 1);
-q = (t.^2)/8 .* (t>0) .* (t<2) + (-0.5*t.^2+3*t-3.5).*(t>2).*(t<3) + 1*(t>3);
+q = (t.^2)/8 .* (t>0) .* (t<2) + (-0.5*t.^2+3*t-3.5).*(t>2).*(t<3 ...
+    ) + 1*(t>3);
 dq = derivative(t, q);
 ddq = derivative(t, dq);
 plot(t,q,'r',t,dq,'b',t,ddq,'g')
